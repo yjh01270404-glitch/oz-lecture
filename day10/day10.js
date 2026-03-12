65e34319b10d494829f8208333e395d86e27fadd
yjh01270404-glitch <yjh01270404@gmail.com>
2026-03-12 16:00:38 +0900

    day10 과제 완료

---
+++ day10/day10.js
@@ -0,0 +1,92 @@
+// // // 이항 산술 연산자
+// // let sum = 10 + 20;
+// // console.log(sum);
+// // let sub = 20 - 10;
+// // console.log(sum);
+// // let multi = 10 * 20;
+// // console.log(multi);
+// // let remain = 10 % 3;
+// // console.log(remain);
+// // let expon = 2 ** 3;
+// // console.log(expon);
+
+// // // 단항 산술 연산자
+// // let increment = 10;
+// // increment++;
+// // console.log(increment);
+// // let decrement = 10;
+// // decrement--;
+// // console.log(decrement);
+
+// // var num = 10;
+// // let subNum = ++num;
+// // console.log(subNum);
+
+// // var num = 10;
+// // let subNum2 = num++;
+// // console.log(subNum2);
+
+// // // 단항 부정 연산자
+// // var num = 10;
+// // num = - num;
+// // console.log(num);
+
+// // // 연산자 우선 순위
+// // var i = 3 + 4 * 5;
+// // console.log(i);
+
+// // var i = (3 + 4) * 5;
+// // console.log(i);
+
+// // 복합 대입 연산자
+// let x = 10,
+//   y = 10,
+//   z = 10;
+// x += 5;
+// console.log(x);
+// y -= 5;
+// console.log(y);
+// z *= 5;
+// console.log(z);
+
+// let score = 90;
+// let grade = score >= 90 ? "A+" : "B";
+// console.log(grade);
+
+// 조건문
+// var num = 10;
+// if (num % 2 === 0) {
+//   console.log("변수 num에 할당된 숫자는 짝수입니다.");
+// } else {
+//   console.log("변수 num에 할당된 숫자는 홀수입니다.");
+// }
+
+// switch문
+// let food = "melon";
+// switch (food) {
+//   case "melon":
+//   case "apple":
+//     console.log("fruit");
+//     break;
+//   case "carrot":
+//     console.log("vegetable");
+//     break;
+//   default:
+//     console.log("it's not fruit and vegetable");
+//     break;
+// }
+
+// if문
+// var score = 88;
+// if (score >= 90) {
+//   console.log("A++학점");
+// } else if (score >= 80 && score < 90) {
+//   console.log("A학점");
+// } else if (score >= 70 && score < 80) {
+//   console.log("B학점");
+// } else {
+//   console.log("학점 없음");
+// }
+
+let input = prompt("점수를 입력하세요.");
+console.log(input);
---
+++ day10/score-calculator/score.js
@@ -0,0 +1,53 @@
+// 변수 선언
+const maxScore = 100;
+let input = prompt("점수를 입력하세요.");
+let score = Number(input);
+var grade;
+score += 5;
+console.log(score);
+
+// 등급 부여
+if (score >= 100) {
+  grade = "S";
+} else if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+} else if (score >= 70) {
+  grade = "C";
+} else if (score >= 60) {
+  grade = "D";
+} else {
+  grade = "F";
+}
+
+console.log("Final Score: " + score);
+console.log("Grade: " + grade);
+
+// 합격/불합격
+const status = score >= 60 ? "Pass" : "Fail";
+
+console.log("Final Score: " + score);
+console.log("Grade: " + grade);
+console.log("Status: " + status);
+
+// 등급별 메시지
+switch (grade) {
+  case "S":
+    console.log("Message: Super!!");
+    break;
+  case "A":
+    console.log("Message: Excellent work!");
+    break;
+  case "B":
+    console.log("Message: Good job!");
+    break;
+  case "C":
+    console.log("Message: Satisfactory performance.");
+    break;
+  case "D":
+    console.log("Message: Needs improvement.");
+    break;
+  default:
+    console.log("Message: Please try harder!");
+}
